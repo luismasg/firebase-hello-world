fdd0ef927f1fa9feb9e5eb64ccdf50671c4ac1fa
Luis Manuel Suarez Gutierrez <lsuarez@tiempodevelopment.com>
2019-10-24 02:21:11 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    user: "",
+    password: "",
+    todo: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  submit = e => {
+    e.preventDefault();
+    // firebase
+    //   .auth()
+    //   .createUserWithEmailAndPassword(this.state.user, this.state.password)
+    //   .then(r => console.log("response : ", r));
+
+    const ref = firebase.firestore();
+    const collection = ref.collection("todo");
+    collection
+      .add({ todo: this.state.todo })
+      .then(response => console.log("que paso?  ,", response));
+  };
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.submit}>
+          <input
+            type="text"
+            value={this.state.user}
+            onChange={this.handleChange}
+            name="user"
+          />
+
+          <input
+            type="text"
+            value={this.state.password}
+            onChange={this.handleChange}
+            name="password"
+          />
+          <button type="submit">sign in</button>
+          <input
+            type="text"
+            value={this.state.todo}
+            name="todo"
+            onChange={this.handleChange}
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import firebase from "firebase/app";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+firebase.initializeApp({
+  apiKey: "AIzaSyBfcaMg-moTyYvgbAwaVQKcRcRBzfIx5mI",
+  authDomain: "cosa1-20764.firebaseapp.com",
+  databaseURL: "https://cosa1-20764.firebaseio.com",
+  projectId: "cosa1-20764",
+  storageBucket: "cosa1-20764.appspot.com",
+  messagingSenderId: "101323988963",
+  appId: "1:101323988963:web:ac72144d38e2a006098ad6"
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
